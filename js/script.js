96eb061ec51ac0f269f6868976b2a14690064c2d
lucacappello79 <postt79@gmail.com>
2023-02-22 15:24:48 +0100

    feat: started adding variables and conditions

--- js/script.js
+++ js/script.js
@@ -8,13 +8,34 @@
 // ALLORA applicare uno sconto del 20% al prezzo totale
 //? SE l'età dell'utente è superiore ai 65 anni 
 // ALLORA applicare uno sconto del 40% al prezzo totale
+//comunicare all'utente la somma totale da pagare
 
 let distanceKm = parseInt(prompt("Inserisci la distanza dalla tua destinazione in KM"));
-let userAge = prompt("Inserisci la tua età");
-
-console.log(distanceKm);
-
+let userAge = parseInt(prompt("Inserisci la tua età"));
 let totalPrice = (distanceKm * (0.21));
+let minorDiscount = (totalPrice / 100 * 20)
+let seniorDiscount = (totalPrice / 100 * 40)
 
-console.log(totalPrice);
+console.log("distanza totale " + distanceKm);
+console.log("prezzo intero " + totalPrice);
+console.log("sconto minorenni " + minorDiscount);
+console.log("sconto senior " + seniorDiscount);
 
+if (userAge < 18) {
+
+    console.log(totalPrice - minorDiscount);
+
+    document.writeln(`L'importo da pagare è di ${(totalPrice - minorDiscount).toFixed(2)} €`)
+
+} else if (userAge > 65) {
+
+    console.log(totalPrice - seniorDiscount) ;
+
+    document.writeln(`L'importo da pagare è di ${(totalPrice - seniorDiscount).toFixed(2)} €`)
+
+
+} else {
+
+    console.log(totalPrice.toFixed(2))
+
+}
